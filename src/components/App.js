5a9960a84cd5340250333b43d3212224b2fde2f7
bomb-on <0nada0@gmail.com>
2018-11-29 17:08:26 +0000

    Initial cleanup

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../static/css/style.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <code> &gt; wallet.conceal.network</code>
+      </div>
+    );
+  }
+}
+
+export default App;
