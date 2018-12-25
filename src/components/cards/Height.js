6d3972d72da7823c147a921f09519efb60f499d0
bomb-on <0nada0@gmail.com>
2018-12-25 16:48:24 +0000

    Showing current blockchain height

--- src/components/cards/Height.js
+++ src/components/cards/Height.js
@@ -1,13 +1,46 @@
 import React from 'react';
 
 
-const Height = (props) => {
-  return (
-    <div className="dash-content">
-      <label className="tx-primary">CURRENT HEIGHT</label>
-      <h2>X</h2>
-    </div>
-  )
-};
+class Height extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      blockchainHeight: 0,
+      updateInterval: 10,  // in seconds
+    };
+
+    this.fetchHeight = this.fetchHeight.bind(this);
+  }
+
+  componentWillMount() {
+    this.fetchHeight();
+  }
+
+  componentDidMount() {
+    this.fetchHeightInterval = setInterval(this.fetchHeight, this.state.updateInterval * 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.fetchHeightInterval);
+  }
+
+  fetchHeight() {
+    fetch('https://explorer.conceal.network/q/height/')
+      .then(r => r.text())
+      .then(blockchainHeight => this.setState({ blockchainHeight }))
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    const { blockchainHeight } = this.state;
+
+    return (
+      <div className="dash-content">
+        <label className="tx-primary">CURRENT HEIGHT</label>
+        <h2>{blockchainHeight.toLocaleString()}</h2>
+      </div>
+    );
+  }
+}
 
 export default Height;
