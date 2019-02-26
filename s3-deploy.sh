7670b7ae198a4cfc49e04961416a4a494bab05a5
Nikolai Danylchyk <n.danylchyk@shoop.de>
2019-02-26 15:56:57 +0000

    Added invalidation

--- s3-deploy.sh
+++ s3-deploy.sh
@@ -7,5 +7,9 @@ DOMAIN="wallet.conceal.network"
 aws s3 rm s3://$DOMAIN/ --recursive
 aws s3 cp ./build s3://$DOMAIN/ --recursive
 
+aws configure set preview.cloudfront true
+aws cloudfront create-invalidation --distribution-id EPW18WLSUWHMU --paths /
+aws cloudfront create-invalidation --distribution-id EPW18WLSUWHMU --paths /index.html
+
 echo "Sucessfully deployed to $DOMAIN"
 exit 0;
