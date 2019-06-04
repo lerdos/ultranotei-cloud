4e99a4a2ec771b81e4b6fa8c23add07e15a04611
bomb-on <0nada0@gmail.com>
2019-06-04 14:03:15 +0000

    WalletDropdown herp-a-derp

--- src/components/elements/WalletDropdown.js
+++ src/components/elements/WalletDropdown.js
@@ -37,9 +37,37 @@ const WalletDropdown = props => {
   }, [wallets]);
 
   return (
-    <div>
-      Balance too low. Send some funds to your wallet to process this payment.
-    </div>
+    <>
+      {walletsLoaded && Object.keys(availableWallets).length > 0 &&
+        <select
+          className="form-control autoWidth"
+          onChange={e => {
+            setWallet(wallets[e.target.value]);
+            setWalletAddress(e.target.value);
+          }}
+          value={walletAddress}
+        >
+          {Object.keys(availableWallets).map(address =>
+            <option value={address} key={address}>
+              {maskAddress(address)} ({wallets[address].balance} CCX)
+            </option>
+          )}
+        </select>
+      }
+      {walletsLoaded && Object.keys(wallets).length > 0 && Object.keys(availableWallets).length === 0 &&
+        <div>
+          Balance too low. Send some funds to your wallet to process this payment.
+        </div>
+      }
+      {walletsLoaded && Object.keys(wallets).length === 0 &&
+        <div>
+          You have no wallets yet. Create one
+          <Button className="btn-uppercase-sm btn-create-wallet" onClick={() => createWallet()}>
+            here
+          </Button>
+        </div>
+      }
+    </>
   )
 };
 
